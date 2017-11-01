1e73bd0a420b31962d7540dc61d4b3f8706374f8
Muhammad Abdullah <muhammadabdullah@appsgenii.eu>
2017-11-01 21:22:56 +0500

    form added for demo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require front/bootstrap/bootstrap.min
+//= require bootstrap-datepicker
+//= require main
 //= require_self
 
-$(document).ready(function() {
-});
+
---
+++ spec/factories/forms.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :form do
+    department "MyString"
+    catogery "MyString"
+    date_started "2017-11-01"
+    date_completed "2017-11-01"
+    summary "MyText"
+  end
+end
